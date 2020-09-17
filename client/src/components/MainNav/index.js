b3a4361786ea1703b5d770f4ce8ed3c2624f9450
hishamss <hishamss90@gmail.com>
2020-09-16 18:20:35 -0700

    added logo in navbar and auth0 login page

--- client/src/components/MainNav/index.js
+++ client/src/components/MainNav/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 const MainNav = () => {
   const {
     loginWithRedirect,
@@ -11,10 +12,13 @@ const MainNav = () => {
     isLoading,
   } = useAuth0();
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar id="nav_container" bg="light" expand="lg">
+      <Navbar.Brand href="#home">
+        <img id="logo_img" src="goMeeting.png" />
+      </Navbar.Brand>
       {/* ml-auto to align Nav to the right */}
       <Nav className="ml-auto">
+        {isAuthenticated && <p>{user.email}</p>}
         {isAuthenticated ? (
           <button onClick={() => logout()}>Logout</button>
         ) : (
