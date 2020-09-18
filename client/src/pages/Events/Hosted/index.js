88c5a0d3aa992aff2f204b2ea142f3cc579f0c14
hishamss <hishamss90@gmail.com>
2020-09-17 17:41:19 -0700

    added bootstrap Modal and added onClick event listner to show the Modal when host event button is clicked

--- client/src/pages/Events/Hosted/index.js
+++ client/src/pages/Events/Hosted/index.js
@@ -1,16 +1,38 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import "./style.css";
 const Hosted = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div id="hostedCont">
-      <div id="hostEventDiv">
-        <Button id="hostBtn">Host Event</Button>
+    <>
+      <div id="hostedCont">
+        <div id="hostEventDiv">
+          <Button id="hostBtn" onClick={() => handleShow()}>
+            Host Event
+          </Button>
+        </div>
+        <div id="listHostedEventsDiv">
+          <div id="hostedEvents"></div>
+        </div>
       </div>
-      <div id="listHostedEventsDiv">
-        <div id="hostedEvents"></div>
-      </div>
-    </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
