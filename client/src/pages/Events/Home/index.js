7520a20d1960c609f341417bd1c58c0da9073387
hishamss <hishamss90@gmail.com>
2020-09-22 22:18:24 -0700

    added attend button on each available event so the user can book the event

--- client/src/pages/Events/Home/index.js
+++ client/src/pages/Events/Home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Button, Modal } from "react-bootstrap";
 import { GraphQlAPI } from "../../../API";
 import { useAuth0 } from "@auth0/auth0-react";
 import { getAllEvents } from "../../../actions";
@@ -9,6 +9,7 @@ import "./style.css";
 const Home = () => {
   const { user } = useAuth0();
   const [allEvents, setAllEvents] = useState([]);
+  const [show, setShow] = useState(false);
   useEffect(() => {
     fetchEvents();
   }, []);
@@ -46,31 +47,93 @@ const Home = () => {
         console.log(err);
       });
   };
+
+  const handleBooking = (meetingId) => {
+    console.log("bookcing clicked", meetingId, user.email);
+    let requestBody = {
+      query: `
+    mutation {
+      addGuest(guestInput: {
+          meetingId: "${meetingId}"
+          email: "${user.email}"
+          
+      }){
+      _id
+      }
+    }
+    `,
+    };
+    GraphQlAPI(requestBody)
+      .then((response) => {
+        if (response.status !== 200 && response.status !== 201) {
+          throw new Error("smth went wrong with loading all events");
+        }
+        return response.json();
+      })
+      .then((res) => {
+        if (res) {
+          setShow(true);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleCloseModal = () => {
+    setShow(false);
+  };
   return (
-    <div id="homeCont" className="text-center">
-      <h1 style={{ margin: "1rem" }}>Available Events</h1>
-      <div id="allEvents">
-        {allEvents
-          .filter((event) => event.host !== user.email)
-          .map((row) => {
-            const dateToFormat = new Date(+row.date); //convert row.date to number by using unary operator
-            return (
-              <Card
-                className="eventCards"
-                style={{ width: "20rem" }}
-                key={row._id}
-              >
-                <Card.Body>
-                  <Card.Title className="cardTitle">{row.name}</Card.Title>
-                  <Card.Text className="cardText">
-                    <Moment>{dateToFormat}</Moment>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            );
-          })}
+    <>
+      <div id="homeCont" className="text-center">
+        <h1 style={{ margin: "1rem" }}>Available Events</h1>
+        <div id="allEvents">
+          {allEvents
+            .filter((event) => event.host !== user.email)
+            .map((row) => {
+              const dateToFormat = new Date(+row.date); //convert row.date to number by using unary operator
+              return (
+                <Card
+                  className="eventCards"
+                  style={{ width: "20rem" }}
+                  key={row._id}
+                >
+                  <Card.Body>
+                    <Card.Title className="cardTitle">{row.name}</Card.Title>
+                    <Card.Text className="cardText">
+                      <Moment>{dateToFormat}</Moment>
+                    </Card.Text>
+                    <Button
+                      className="bookEvent"
+                      onClick={() => handleBooking(row._id)}
+                    >
+                      Attend
+                    </Button>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+        </div>
       </div>
-    </div>
+      <Modal show={show} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Event Added</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <h1>The Event has been added to your list</h1>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            style={{ fontSize: "1.2rem" }}
+            id="closeModal"
+            variant="secondary"
+            onClick={handleCloseModal}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
