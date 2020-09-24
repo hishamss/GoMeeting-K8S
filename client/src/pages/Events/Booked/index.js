471db815a990f91d96b7993dad48c8dca4d2c151
hishamss <hishamss90@gmail.com>
2020-09-23 22:39:31 -0700

    display the booked events in <p> temporarely

--- client/src/pages/Events/Booked/index.js
+++ client/src/pages/Events/Booked/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./style.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import { GraphQlAPI } from "../../../API";
+import Moment from "react-moment";
 const Booked = () => {
   const { user } = useAuth0();
   const [bookedEvents, setBookedEvents] = useState([]);
@@ -40,7 +41,17 @@ const Booked = () => {
   return (
     <div id="bookedCont" className="text-center">
       <h1 style={{ margin: "1rem" }}>Booked Events</h1>
-      <div id="bookedEvents"></div>
+      <div id="bookedEvents">
+        {bookedEvents.map((row) => {
+          console.log(row["meeting"]);
+          const dateToFormat = new Date(+row["meeting"].date);
+          return (
+            <p key={row["meeting"]._id}>
+              Name:{row["meeting"].name}, date: <Moment>{dateToFormat}</Moment>
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 };
