404218feda6698e007ba1683faa8d8cea55fd6e0
hishamss <hishamss90@gmail.com>
2020-09-23 20:58:36 -0700

    create link and page for booked events for the user

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from "./pages/Landing";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Hosted from "./pages/Events/Hosted";
 import Home from "./pages/Home";
+import Booked from "./pages/Events/Booked";
 import { useAuth0 } from "@auth0/auth0-react";
 function App() {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -21,6 +22,7 @@ function App() {
         {isAuthenticated && <Route exact path="/home" component={Home} />}
         {!isAuthenticated && <Route exact path="/login" component={Landing} />}
         {isAuthenticated && <Route exact path="/hosted" component={Hosted} />}
+        {isAuthenticated && <Route exact path="/booked" component={Booked} />}
         {isAuthenticated && <Route path="/" component={Home} />}
       </Switch>
     </>
---
+++ client/src/pages/Events/Booked/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const Booked = () => {
+  return <div>Booked Pages</div>;
+};
+
+export default Booked;
